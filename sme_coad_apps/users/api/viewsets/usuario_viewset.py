c084251878910c663f85330bf1fa555e0c48b58c
wesley1263 <weslei.souza@amcom.com.br>
2019-09-18 14:53:31 -0300

    Implementa importação Implementa ajustes de codeclimate viewsets

--- sme_coad_apps/users/api/viewsets/usuario_viewset.py
+++ sme_coad_apps/users/api/viewsets/usuario_viewset.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 
-from sme_coad_apps.users.api.serializers.usuario_serializer import UsuarioSerializer
-from sme_coad_apps.users.models import User
+from ..serializers.usuario_serializer import UsuarioSerializer
+from ...models import User
 
 
 class UsuarioViewSet(viewsets.ModelViewSet):
