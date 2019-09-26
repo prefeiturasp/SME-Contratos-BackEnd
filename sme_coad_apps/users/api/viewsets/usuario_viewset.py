96c6984023cacc1359168939dca954fadcf20fce
wesley1263 <weslei.souza@amcom.com.br>
2019-09-26 16:41:11 -0300

    Este commit: - Implementa o endpoint para usuário no primeiro acesso.

--- sme_coad_apps/users/api/viewsets/usuario_viewset.py
+++ sme_coad_apps/users/api/viewsets/usuario_viewset.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 
 from ..serializers.usuario_serializer import (UsuarioSerializer, UsuarioSerializerCreators)
 
+user_model = get_user_model()
+
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     lookup_field = 'username'
@@ -19,6 +21,13 @@ class UsuarioViewSet(viewsets.ModelViewSet):
         else:
             return UsuarioSerializerCreators
 
+    @action(detail=True, url_path='primeiro-acesso', methods=['get'], permission_classes=[AllowAny])
+    def primeiro_acesso(self, request, username=None):
+        serializer = UsuarioSerializer().validate_validado(username)
+        if serializer:
+            return Response({'detail': 'Pode acessar o sistema', 'alterar': False}, status=status.HTTP_200_OK)
+        return Response({'detail': 'Deve alterar a senha padrão', 'alterar': True}, status=status.HTTP_401_UNAUTHORIZED)
+
     @action(detail=True, url_path='troca-senha', methods=['patch'], permission_classes=[AllowAny])
     def troca_senha(self, request, username):
         usuario = request.user
@@ -28,4 +37,3 @@ class UsuarioViewSet(viewsets.ModelViewSet):
         usuario.validado = True
         usuario.save()
         return Response({'detail': 'Usuário validado', 'status': status.HTTP_200_OK})
-
