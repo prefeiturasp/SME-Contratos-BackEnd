301bba2825d1175e9023c4d564ff3398022c297c
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-10-04 11:59:53 -0300

    Implementa filtros na API de contratos por diversos campos.
    Implementa na API de contratos filtros por Situação, Tipo de Serviço,
    Gestor, Empresa, Estado e Termo de Contrato.
    
    Implementa também busca por processo e ordenação por data da ordem de
    início.

--- sme_coad_apps/contratos/tests/test_viewset_contrato.py
+++ sme_coad_apps/contratos/tests/test_viewset_contrato.py
@@ -19,3 +19,12 @@ def test_contrato_view_set(fake_user):
     response = contrato_detalhe(request, uuid=contrato.uuid)
 
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_contrato_view_set_filters(fake_user):
+    contrato_viewset = ContratoViewSet()
+
+    assert contrato_viewset.ordering_fields == ('data_ordem_inicio',)
+    assert contrato_viewset.filter_fields == (
+        'situacao', 'tipo_servico', 'gestor', 'empresa_contratada', 'estado_contrato', 'termo_contrato')
+    assert contrato_viewset.search_fields == ('processo',)
