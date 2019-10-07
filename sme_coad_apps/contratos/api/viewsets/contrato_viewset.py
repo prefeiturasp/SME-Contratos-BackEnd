234eee472aedbc34d7869fdb94e632fec77185b5
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-10-07 09:14:16 -0300

    Cria filtro por range de data de encerramento na API de contrato
    Usando os parametros encerramento_de e encerramento_ate na URL de
    consulta de contratos é possível obter lista dos contratos com
    encerramento entre essas datas.

--- sme_coad_apps/contratos/api/viewsets/contrato_viewset.py
+++ sme_coad_apps/contratos/api/viewsets/contrato_viewset.py
@@ -15,3 +15,21 @@ class ContratoViewSet(ComHistoricoReadOnlyViewSet):
     filter_fields = ('situacao', 'tipo_servico', 'gestor', 'empresa_contratada', 'estado_contrato', 'termo_contrato')
     ordering_fields = ('data_ordem_inicio',)
     search_fields = ('processo',)
+
+    def get_queryset(self):
+        contratos = Contrato.objects \
+            .select_related('empresa_contratada') \
+            .select_related('nucleo_responsavel') \
+            .select_related('gestor') \
+            .select_related() \
+            .all()
+
+        queryset = contratos
+
+        encerramento_de = self.request.query_params.get('encerramento_de')
+        encerramento_ate = self.request.query_params.get('encerramento_ate')
+
+        if encerramento_de is not None and encerramento_ate is not None:
+            queryset = queryset.filter(data_encerramento__range=[encerramento_de, encerramento_ate])
+
+        return queryset
