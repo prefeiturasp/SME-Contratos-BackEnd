d554b9c64df2b9adbf433df19ebd94c4ca99c2d1
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-10-07 10:57:04 -0300

    Cria na api recurso para consulta de Tipos de Serviço
    Fica disponivel o endpoint /servicos/lookup/

--- sme_coad_apps/contratos/urls.py
+++ sme_coad_apps/contratos/urls.py
@@ -2,9 +2,11 @@ from django.urls import path, include
 from rest_framework import routers
 
 from .api.viewsets.contrato_viewset import ContratoViewSet
+from .api.viewsets.tipo_servico_viewsets import TipoServicoViewSet
 
 router = routers.DefaultRouter()
 
+router.register('tipos-servico', TipoServicoViewSet)
 router.register('contratos', ContratoViewSet)
 
 urlpatterns = [
