63d5810ad10012fb029d34f4dc6922862995f373
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-10-29 14:45:37 -0300

    Add informações de usuários na API de cargos nível COAD
    Acrescenta aos retornos da api de cargos, informações do usuário como
    nome, username e uuid.

--- sme_coad_apps/core/api/serializers/coad_serializer.py
+++ sme_coad_apps/core/api/serializers/coad_serializer.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 
 from .coad_assessor_serializer import CoadAssessorSerializer
 from ...models import Coad
+from ....users.api.serializers.usuario_serializer import UsuarioLookUpSerializer
 
 
 class CoadSerializer(serializers.ModelSerializer):
     assessores = CoadAssessorSerializer(many=True)
+    coordenador = UsuarioLookUpSerializer()
 
     class Meta:
         model = Coad
--- sme_coad_apps/core/api/viewsets/coad_assessor_viewset.py
+++ sme_coad_apps/core/api/viewsets/coad_assessor_viewset.py
@@ -1,4 +1,4 @@
-from ..serializers.coad_assessor_serializer import CoadAssessorSerializer
+from ..serializers.coad_assessor_serializer import CoadAssessorSerializer, CoadAssessorCreateSerializer
 
 from ...models import CoadAssessor
 from ...viewsets_abstracts import ComHistoricoViewSet
@@ -7,3 +7,11 @@ from ...viewsets_abstracts import ComHistoricoViewSet
 class CoadAssessorViewSet(ComHistoricoViewSet):
     queryset = CoadAssessor.objects.all()
     serializer_class = CoadAssessorSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return CoadAssessorSerializer
+        elif self.action == 'list':
+            return CoadAssessorSerializer
+        else:
+            return CoadAssessorCreateSerializer
