16bc0fd2a7e931ab30a6ee5fec15f2efc759cc4a
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-11-02 16:08:01 -0300

    Implementa serviços para limpar e gravar servidores de um núcleo

--- sme_coad_apps/core/api/viewsets/nucleo_viewsets.py
+++ sme_coad_apps/core/api/viewsets/nucleo_viewsets.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 
 from ..serializers.nucleo_serializer import NucleoSerializer, NucleoLookUpSerializer, NucleoCreateSerializer
 from ...models import Nucleo
+from ...services import limpa_servidores_nucleo, update_servidores_nucleo
 from ...viewsets_abstracts import ComHistoricoViewSet
 
 
@@ -22,3 +23,17 @@ class NucleoViewSet(ComHistoricoViewSet):
     @action(detail=False)
     def lookup(self, _):
         return Response(NucleoLookUpSerializer(self.queryset, many=True).data)
+
+    @action(detail=True, url_path='limpa-servidores', methods=['delete'])
+    def limpa_servidores(self, _, uuid=None):
+        nucleo = self.get_object()
+        limpa_servidores_nucleo(nucleo=nucleo)
+
+        return Response({'message': f'Todos os servidores do Nucleo {nucleo.sigla} foram apagados'})
+
+    @action(detail=True, url_path='update-servidores', methods=['post'])
+    def update_servidores(self, request, uuid=None):
+        nucleo = self.get_object()
+        servidores = request.data['servidores']
+        update_servidores_nucleo(servidores=servidores, nucleo=nucleo)
+        return Response([{'message': f'Servidores adicionados ao núcleo {nucleo.sigla}'}, {'servidores': servidores}])
--- sme_coad_apps/core/models/servidor.py
+++ sme_coad_apps/core/models/servidor.py
@@ -1,11 +1,10 @@
-from django.db import models
-
 from auditlog.models import AuditlogHistoryField
 from auditlog.registry import auditlog
+from django.db import models
 
 from sme_coad_apps.users.models import User
-from ..models_abstracts import ModeloBase
 from .nucleo import Nucleo
+from ..models_abstracts import ModeloBase
 
 
 class Servidor(ModeloBase):
@@ -15,6 +14,22 @@ class Servidor(ModeloBase):
 
     def __str__(self):
         return f"{self.servidor.nome}"
+
+    @classmethod
+    def append_servidores(cls, nucleo=None, servidores=[]):
+        if not nucleo or not servidores:
+            return
+
+        for servidor in servidores:
+            usuario = User.objects.get(username=servidor["username"])
+            cls.objects.create(nucleo=nucleo, servidor=usuario)
+
+    @classmethod
+    def limpa_servidores(cls, nucleo=None):
+        if not nucleo:
+            return
+
+        cls.objects.filter(nucleo=nucleo).delete()
 
     class Meta:
         verbose_name = "Servidor"
