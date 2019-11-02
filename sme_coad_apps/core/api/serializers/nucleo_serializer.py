3d1c9eaba844ea0de3176e8723f9615e75ac74a6
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-11-02 11:31:43 -0300

    Add ao serializer de Nucleo informações de chefe e suplente

--- sme_coad_apps/core/api/serializers/nucleo_serializer.py
+++ sme_coad_apps/core/api/serializers/nucleo_serializer.py
@@ -2,12 +2,17 @@ from rest_framework import serializers
 
 from ..serializers.divisao_serializer import DivisaoLookUpSerializer
 from ...models import Nucleo
+from ....users.api.serializers.usuario_serializer import UsuarioLookUpSerializer
 
 
 class NucleoSerializer(serializers.ModelSerializer):
+    divisao = DivisaoLookUpSerializer()
+    chefe = UsuarioLookUpSerializer()
+    suplente_chefe = UsuarioLookUpSerializer()
+
     class Meta:
         model = Nucleo
-        fields = ('id', 'sigla', 'nome')
+        fields = ('id', 'sigla', 'nome', 'chefe', 'suplente_chefe', 'divisao', 'uuid')
 
 
 class NucleoLookUpSerializer(serializers.ModelSerializer):
