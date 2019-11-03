6ddb81d533f290ac78d3de0e07c3f306401c3657
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-11-03 16:58:55 -0300

    Cria endpoint que retorna servidores de um núcleo
    Implementa na api de nucleos uma action para retornar os servidores
    cadastrados no núcleo.
    
    /nucleos/{uuid}/servidores/

--- sme_coad_apps/core/tests/test_nucleo_endpoints.py
+++ sme_coad_apps/core/tests/test_nucleo_endpoints.py
@@ -1,7 +1,20 @@
 import pytest
+from model_mommy import mommy
 from rest_framework import status
 
+from ..models import Nucleo, Servidor
+
 pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def nucleo(fake_user):
+    return mommy.make(Nucleo, id=1, sigla='nc1', nome='nucleo teste', chefe=fake_user, suplente_chefe=fake_user)
+
+
+@pytest.fixture
+def servidor(fake_user, nucleo):
+    return mommy.make(Servidor, nucleo=nucleo, servidor=fake_user)
 
 
 def test_url_unaunthorized(client):
@@ -17,3 +30,21 @@ def test_url_authorized(authencticated_client):
 def test_url_lookup(authencticated_client):
     response = authencticated_client.get('/nucleos/lookup/')
     assert response.status_code == status.HTTP_200_OK
+
+
+def test_url_servidores(authencticated_client, fake_user, nucleo):
+    response = authencticated_client.get(f'/nucleos/{nucleo.uuid}/servidores/')
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_url_limpa_servidores(authencticated_client, fake_user, nucleo):
+    response = authencticated_client.delete(f'/nucleos/{nucleo.uuid}/limpa-servidores/')
+    assert response.status_code == status.HTTP_200_OK
+
+# TODO Fazer funcionar esse teste
+# def test_url_update_servidores(authencticated_client, fake_user, nucleo, servidor):
+#     servidor_json = ServidorSerializer(servidor, many=True).data
+#     # servidores = [json.dumps(servidor_json), ]
+#     response = authencticated_client.post(f'/nucleos/{nucleo.uuid}/update-servidores/',
+#     servidor_json, content_type="application/json")
+#     assert response.status_code == status.HTTP_200_OK
