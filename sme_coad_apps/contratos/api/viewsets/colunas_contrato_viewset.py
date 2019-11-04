c4b6a860d3296b811c71deeb84da7c4eea851ddf
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-11-04 14:03:38 -0300

    Corrige api colunas de contrato para retornar por usuário
    Corrige a API para retornar as colunas de contrato dependendo do
    usuário logado.

--- sme_coad_apps/contratos/api/viewsets/colunas_contrato_viewset.py
+++ sme_coad_apps/contratos/api/viewsets/colunas_contrato_viewset.py
@@ -1,15 +1,17 @@
-from rest_framework.decorators import action
-from rest_framework.response import Response
-
 from ..serializers.colunas_contrato_serializer import ColunasContratoSerializer
 from ...models.colunas_contrato import ColunasContrato
 from ....core.viewsets_abstracts import ComHistoricoViewSet
+
 
 class ColunasContratoViewSet(ComHistoricoViewSet):
     lookup_field = 'uuid'
     queryset = ColunasContrato.objects.all()
     serializer_class = ColunasContratoSerializer
 
-    # @action(detail=False)
-    # def lookup(self, _):
-    #     return
+    def get_queryset(self):
+        return ColunasContrato.objects.filter(usuario__id=self.request.user.id)
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = queryset.get(pk=self.request.user.id)
+        return obj
