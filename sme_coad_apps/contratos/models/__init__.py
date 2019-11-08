927d4bb1238778cf062ceebe3e97b44e648f3c3e
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-11-08 16:53:25 -0300

    Cria modelo de parâmetros de alertas de vigencia de contratos

--- sme_coad_apps/contratos/models/__init__.py
+++ sme_coad_apps/contratos/models/__init__.py
@@ -1,4 +1,5 @@
-from .contrato import Contrato, ContratoUnidade
 from .colunas_contrato import ColunasContrato
+from .contrato import Contrato, ContratoUnidade
 from .empresa import Empresa
+from .parametros import ParametrosNotificacoesVigencia
 from .tipo_servico import TipoServico
