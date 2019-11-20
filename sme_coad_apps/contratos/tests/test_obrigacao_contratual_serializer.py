d7ec7ee9e615c0413e203a368cabaa55593fcbb4
Kelwy Oliveira <kelwyduarte@gmail.com>
2019-11-20 12:26:11 -0300

    Adiciona teste de serialize

---
+++ sme_coad_apps/contratos/tests/test_obrigacao_contratual_serializer.py
@@ -0,0 +1,20 @@
+import pytest
+from model_mommy import mommy
+
+from ..api.serializers.obrigacao_contratual_serializer import ObrigacaoContratualSerializer
+from ..models.obrigacao_contratual import ObrigacaoContratual
+from ..models.contrato import Contrato
+
+pytestmark = pytest.mark.django_db
+
+
+def test_cobrigacao_contratual_serializer():
+    contrato = mommy.make('Contrato')
+    obrigacao_contratual = mommy.make('ObrigacaoContratual', contrato=contrato, item='01', obrigacao='teste')
+
+    serializer = ObrigacaoContratualSerializer(obrigacao_contratual)
+
+    assert serializer.data is not None
+    assert serializer.data['contrato']
+    assert serializer.data['item']
+    assert serializer.data['obrigacao']
