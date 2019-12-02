61085753605573c710e648aa4fee3ced8fb781a7
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-12-02 11:57:38 -0300

    Remove campo DRE do serializer de Unidades do Contrato
    O campo DRE passa a ser trazido do cadastro de unidades e não deve ser
    gravado na tabela de unidades do contrato.

--- sme_coad_apps/contratos/tests/test_contrato_unidade_serializer.py
+++ sme_coad_apps/contratos/tests/test_contrato_unidade_serializer.py
@@ -24,8 +24,7 @@ def unidade(dre):
 @pytest.fixture
 def contrato_unidade(contrato, unidade):
     return mommy.make('ContratoUnidade', contrato=contrato, unidade=unidade, valor_mensal=2000.00,
-                      valor_total=10000.00, lote='Lote teste',
-                      dre_lote='DTC')
+                      valor_total=10000.00, lote='Lote teste')
 
 
 def test_contrato_unidade_serializer(contrato, unidade, contrato_unidade):
@@ -38,7 +37,6 @@ def test_contrato_unidade_serializer(contrato, unidade, contrato_unidade):
     assert serializer.data['valor_mensal']
     assert serializer.data['valor_total']
     assert serializer.data['lote']
-    assert serializer.data['dre_lote']
 
 
 def test_contrato_unidade_lookup_serializer(contrato, unidade, contrato_unidade):
@@ -50,5 +48,4 @@ def test_contrato_unidade_lookup_serializer(contrato, unidade, contrato_unidade)
     assert serializer.data['valor_mensal']
     assert serializer.data['valor_total']
     assert serializer.data['lote']
-    assert serializer.data['dre_lote']
     assert serializer.data['unidade']['dre']
