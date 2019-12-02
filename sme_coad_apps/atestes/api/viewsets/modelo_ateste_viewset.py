11bd577022d90afca89afb1324b230792e5ec071
Kelwy Oliveira <kelwyduarte@gmail.com>
2019-12-02 10:00:44 -0300

    Adiciona lookup para titulo do modelo

--- sme_coad_apps/atestes/api/viewsets/modelo_ateste_viewset.py
+++ sme_coad_apps/atestes/api/viewsets/modelo_ateste_viewset.py
@@ -1,9 +1,15 @@
-from ..serializers.modelo_ateste_serializer import ModeloAtesteSerializer
+from ..serializers.modelo_ateste_serializer import ModeloAtesteSerializer, ModeloAtesteLookUpSerializer
 from ...models.modelo_ateste import ModeloAteste
-from ....core.viewsets_abstracts import ComHistoricoViewSet, ComHistoricoReadOnlyViewSet
+from ....core.viewsets_abstracts import ComHistoricoViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class ModeloAtesteViewSet(ComHistoricoViewSet):
     lookup_field = 'uuid'
     queryset = ModeloAteste.objects.all()
     serializer_class = ModeloAtesteSerializer
+
+    @action(detail=False, url_path='titulos-modelo-ateste')
+    def lookup(self, _):
+        return Response(ModeloAtesteLookUpSerializer(self.queryset.order_by('-criado_em'), many=True).data)
