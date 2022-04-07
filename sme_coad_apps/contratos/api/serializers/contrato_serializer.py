9b49b97e0d623de013d3cb3535a173aa6fd16411
Diogo Santos <euamofisica2006@hotmail.com>
2022-04-07 11:43:19 -0300

    altera serializers relacionada a objeto do contrato e edital

--- sme_coad_apps/contratos/api/serializers/contrato_serializer.py
+++ sme_coad_apps/contratos/api/serializers/contrato_serializer.py
@@ -243,7 +243,7 @@ class ContratoSimplesSerializer(serializers.ModelSerializer):
         return obj.data_encerramento.strftime('%d/%m/%Y')
 
     def get_objeto(self, obj):
-        return obj.tipo_servico.nome
+        return obj.tipo_servico.nome if obj.tipo_servico else None
 
     class Meta:
         model = Contrato
