1c466ce0b1ab2974e6734805a8372dbaf6138873
Luis <lhzimmermann@gmailc.om>
2022-05-10 17:58:10 -0300

    revert config

--- config/settings/base.py
+++ config/settings/base.py
@@ -14,7 +14,7 @@ APPS_DIR = ROOT_DIR.path("sme_coad_apps")
 
 env = environ.Env()
 
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=True)
+READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
 if READ_DOT_ENV_FILE:
     # OS environment variables take precedence over variables from .env
     env.read_env(str(ROOT_DIR.path(".env")))
