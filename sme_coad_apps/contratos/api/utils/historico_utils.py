714dafe91a8800dc275ba632f5e3d2265a9cd17a
Luis <lhzimmermann@gmailc.om>
2022-05-27 15:16:19 -0300

    ajustes

--- sme_coad_apps/contratos/api/utils/historico_utils.py
+++ sme_coad_apps/contratos/api/utils/historico_utils.py
@@ -3,7 +3,7 @@ import json
 
 def serializa_historico(log_entry):
     historico = []
-    for reg in log_entry.all():
+    for reg in log_entry.all().reverse():
         registro = {}
 
         mudancas = json.loads(reg.changes)
@@ -20,10 +20,12 @@ def serializa_historico(log_entry):
             'email': reg.actor.email,
             'uuid': str(reg.actor.uuid)
         } if reg.actor else None
-        registro['created_at'] = reg.timestamp.strftime('%m/%d/%Y %H:%M:%S')
+        data = reg.timestamp.strftime('%d-%m-%Y %H:%M:%S')
         if reg.action == 0:
+            registro['created_at'] = data
             registro['action'] = 'CREATE'
         elif reg.action == 1:
+            registro['updated_at'] = data
             registro['action'] = 'UPDATE'
 
         historico.append(registro)
