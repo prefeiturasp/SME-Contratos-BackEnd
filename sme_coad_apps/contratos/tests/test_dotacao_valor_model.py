7aa78329dfaaeabd2972f9f3731ddb81f66c980b
Kelwy Oliveira <kelwyduarte@gmail.com>
2022-05-30 17:21:24 -0300

    Atualiza e adiciona testes unitários referente a dotação e empenho

--- sme_coad_apps/contratos/tests/test_dotacao_valor_model.py
+++ sme_coad_apps/contratos/tests/test_dotacao_valor_model.py
@@ -4,23 +4,30 @@ from model_mommy import mommy
 
 from ..admin import DotacaoValorAdmin
 from ..models import Contrato, DotacaoValor
+from ..models.dotacao_valor import DotacaoOrcamentaria, Empenho
 
 pytestmark = pytest.mark.django_db
 
 
-def test_instance_model():
+def test_instance_model(dotacao_orcamentaria):
     contrato = mommy.make('Contrato')
-    dotacao_valor = mommy.make('DotacaoValor', contrato=contrato, dotacao_orcamentaria='1.1', valor='2000')
+    dotacao_valor = mommy.make('DotacaoValor', contrato=contrato, dotacao_orcamentaria=dotacao_orcamentaria,
+                               valor='2000')
+    empenho = mommy.make('Empenho', dotacao_valor=dotacao_valor, numero='123456', valor_previsto=1000)
 
     assert isinstance(dotacao_valor, DotacaoValor)
     assert isinstance(dotacao_valor.contrato, Contrato)
-    assert isinstance(dotacao_valor.dotacao_orcamentaria, str)
+    assert isinstance(dotacao_valor.dotacao_orcamentaria, DotacaoOrcamentaria)
     assert isinstance(dotacao_valor.valor, str)
 
+    assert isinstance(empenho, Empenho)
+    assert isinstance(empenho.dotacao_valor, DotacaoValor)
+    assert isinstance(empenho.numero, str)
 
-def test_srt_model():
+
+def test_srt_model(dotacao_orcamentaria):
     contrato = mommy.make('Contrato')
-    model = mommy.make('DotacaoValor', contrato=contrato, dotacao_orcamentaria='1.1', valor='2000')
+    model = mommy.make('DotacaoValor', contrato=contrato, dotacao_orcamentaria=dotacao_orcamentaria, valor='2000')
     assert model.__str__() == f'{contrato.termo_contrato} - {model.dotacao_orcamentaria} - R${model.valor}'
 
 
