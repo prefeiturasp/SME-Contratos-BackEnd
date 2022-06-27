31b2193e706261a2609bc2b93d6884555e8d2932
Diogo Santos <euamofisica2006@hotmail.com>
2022-06-27 11:35:17 -0300

    ajusta arquivo de migração

--- sme_coad_apps/atestes/migrations/0003_auto_20220622_0911.py
+++ sme_coad_apps/atestes/migrations/0003_auto_20220622_0911.py
@@ -6,6 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('contratos', '0033_remove_contrato_modelo_ateste'),
         ('atestes', '0002_auto_20191129_1718'),
     ]
 
