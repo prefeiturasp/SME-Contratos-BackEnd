d2f8e0f898d42f187cb27b4cdccc681d06b16c6b
Diogo Igarassu <46908978+DiogoIgarassu@users.noreply.github.com>
2022-06-27 15:23:52 -0300

    Update 0036_auto_20220627_1202.py
    corrige palavra migrations

--- sme_coad_apps/contratos/migrations/0036_auto_20220627_1202.py
+++ sme_coad_apps/contratos/migrations/0036_auto_20220627_1202.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        mmigrations.AlterField(
+        migrations.AlterField(
             model_name='edital',
             name='objeto',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, related_name='objetos', to='contratos.Objeto', verbose_name='Categoria de objeto'),
